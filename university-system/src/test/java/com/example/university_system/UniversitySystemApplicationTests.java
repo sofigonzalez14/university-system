e69bdcf16da00c4f641c2b447eaca1877001bd36
sofigonzalez14 <sofimacag14@gmail.com>
2024-10-29 23:58:30 -0300

    primer commit

---
+++ university-system/src/test/java/com/example/university_system/UniversitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.university_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
